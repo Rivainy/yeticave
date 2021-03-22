7bfdc77a0a665429b6ee4efbca1eed664546cb73
Rivainy <varecka.mart@gmail.com>
2021-03-22 20:19:31 +0300

    Добавлен вывод лотов из массива

--- index.php
+++ index.php
@@ -121,17 +121,18 @@ $adverts = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+            <?php foreach ($adverts as $key => $value) { ?>             
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?= $value['img_url']; ?>" width="350" height="260" alt="<?= $value['name']; ?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?= $value['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $value['name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $value['price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
@@ -139,6 +140,7 @@ $adverts = [
                     </div>
                 </div>
             </li>
+            <?php ;} ?>
         </ul>
     </section>
 </main>
